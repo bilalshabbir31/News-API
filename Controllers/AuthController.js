d93f271dcb1cc52cafbb4ed7463501fc3483c1f4
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-10-12 14:50:39 +0400

    add login method in auth controller

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -1,7 +1,9 @@
 import prisma from "../config/db.js";
 import vine, { errors } from "@vinejs/vine";
-import { registerSchema } from "../validations/authValidation.js";
+import { loginSchema, registerSchema } from "../validations/authValidation.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
 
 const register = async (req, res) => {
   try {
@@ -12,13 +14,15 @@ const register = async (req, res) => {
     // Check if email exist
 
     const findUser = await prisma.users.findUnique({
-      where: { email: payload.email }
+      where: { email: payload.email },
     });
 
     if (findUser) {
-      return res.status(400).json({errors: {
-        email: "Email Already taken. please choose another one."
-      }})
+      return res.status(400).json({
+        errors: {
+          email: "Email Already taken. please choose another one.",
+        },
+      });
     }
 
     // Encrypt Password
@@ -43,4 +47,55 @@ const register = async (req, res) => {
   }
 };
 
-export { register };
+const login = async (req, res) => {
+  try {
+    const body = req.body;
+    const validator = vine.compile(loginSchema);
+    const payload = await validator.validate(body);
+
+    const findUser = await prisma.users.findUnique({
+      where: { email: payload.email },
+    });
+    if (findUser) {
+      if (!bcrypt.compareSync(payload.password, findUser.password)) {
+        return res.status(400).json({
+          errors: {
+            email: "Password is incorrect.",
+          },
+        });
+      }
+
+      // generate token
+      const payloadData = {
+        id: findUser.id,
+        email: findUser.email,
+        name: findUser.name,
+      };
+
+      const token = jwt.sign(payloadData, process.env.JWT_SECERT, {
+        expiresIn: "1d",
+      });
+
+      return res.json({
+        message: "Logged In",
+        access_token: `Bearer ${token}`,
+      });
+    } else {
+      return res.status(400).json({
+        errors: {
+          email: "Email or Password is incorrect.",
+        },
+      });
+    }
+  } catch (error) {
+    if (error instanceof errors.E_VALIDATION_ERROR) {
+      return res.status(400).json({ errors: error.messages });
+    } else {
+      return res
+        .status(500)
+        .json({ status: 500, message: "Something went wrong" });
+    }
+  }
+};
+
+export { register, login };
