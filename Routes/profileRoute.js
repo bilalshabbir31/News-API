a8bf86f946a8e462f6f8ca90e9082fcd177726a4
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-10-12 17:18:59 +0400

    add update route

--- Routes/profileRoute.js
+++ Routes/profileRoute.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { index } from "../Controllers/ProfileController.js";
+import { index, update } from "../Controllers/ProfileController.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
 
 const profileRouter = Router();
 profileRouter.use(authMiddleware);
 profileRouter.get("/profile", index);
+profileRouter.put('/profile/:id', update);
 
 export default profileRouter;
