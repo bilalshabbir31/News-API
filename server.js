5e9ef31365edc569b389aa8a95b1af029d48fc0b
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-10-12 14:13:53 +0400

    add json middlewares

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 import express from "express";
 import "dotenv/config";
+import authRouter from "./Routes/authRoute.js";
 
 const app = express()
 const PORT = process.env.PORT || 8080
 
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.status(200).send({message: "Hello bilal" })
-})
+});
+
+app.use('/api', authRouter);
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
