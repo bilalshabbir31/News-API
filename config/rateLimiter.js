b96287b25d8273a89ab987129487346b4141d39d
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-10-13 12:18:28 +0400

    remove extra spaces

--- config/rateLimiter.js
+++ config/rateLimiter.js
@@ -1,6 +1,5 @@
 import rateLimit from "express-rate-limit";
 
-
 export const limiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 1 minutes
   limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
