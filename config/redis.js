31b469e45c9f816a7ac9c30e38a116f7838be20f
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-10-25 13:46:46 +0400

    fixed

--- config/redis.js
+++ config/redis.js
@@ -3,10 +3,9 @@ import redis from "express-redis-cache";
 const redisCache = redis({
   port: process.env.REDIS_PORT,
   host: process.env.REDIS_HOST,
-  password: process.env.REDIS_PASSWORD,
+  auth_pass: process.env.REDIS_PASSWORD,
   prefix: "backend",
   expire: 60 * 60,
-  enableReadyCheck: true,
 });
 
 export default redisCache;
